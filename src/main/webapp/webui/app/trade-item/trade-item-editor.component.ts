1396d58ce077c2a67891088c7f7fb315862f7db5
Rafael Santos <rafael.santos.bra@gmail.com>
2016-07-15 22:18:44 -0400

    Better style for trade-editor.component.ts

--- src/main/webapp/webui/app/trade-item/trade-item-editor.component.ts
+++ src/main/webapp/webui/app/trade-item/trade-item-editor.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import {Subject} from 'rxjs/Subject';
+import {Observable} from 'rxjs/Observable';
+
 import {TradeItem} from './trade-item';
 import {TradeItemService} from './trade-item.service';
 
@@ -11,7 +14,9 @@ import {TradeItemService} from './trade-item.service';
 })
 export class TradeItemEditorComponent implements OnInit {
   private tradeItem: TradeItem;
-  
+  private toogleClass: string = 'disabled';
+  private uiState: number = 0; // bitwase | 0 = clean; 1 = dirty; 2 = saved;
+
   constructor(private route: ActivatedRoute, private tradeItemService: TradeItemService) { }
 
   public ngOnInit() {
@@ -19,11 +24,29 @@ export class TradeItemEditorComponent implements OnInit {
     this.tradeItemService.get(tradeItemId)
       .then(response => this.tradeItem = response)
       .catch(error => console.log(error));
+
   }
 
-  private save(t: TradeItem):void {
+  private save(t: TradeItem): void {
+    if (!(this.uiState == 1)) {
+      return;
+    }
     this.tradeItemService.save(t)
-      .then( response => this.tradeItem = response )
-      .catch( error => console.log(error));
+      .then(response => {
+        this.tradeItem = response;
+        this.setUiState(2);
+      }
+      ).catch(error => console.log(error));
   }
+
+  private setUiState(n: number): void {
+    if ( n == 0 || n == 2) {
+      this.toogleClass = 'disabled';
+    } else if ( n == 1) {
+      this.toogleClass = 'enabled';
+    }
+
+    this.uiState = n;
+  }
+
 }
