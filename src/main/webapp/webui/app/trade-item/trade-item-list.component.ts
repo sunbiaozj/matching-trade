29fe435bbb9cda5df06423312993991f899520bb
Rafael Santos <rafael.santos.bra@gmail.com>
2016-09-22 00:27:13 -0400

    Pagination will not load page if the user click on the active page.

--- src/main/webapp/webui/app/trade-item/trade-item-list.component.ts
+++ src/main/webapp/webui/app/trade-item/trade-item-list.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 
 import {MessengerService} from '../common/messenger/messenger.service';
 import {RouterService} from '../common/router/router.service';
-import {Pagination} from '../common/http/response-result';
+import {Pagination, Page} from '../common/http/response-result';
 import {TradeItem} from './trade-item';
 import {TradeItemEditorComponent} from './trade-item-editor.component';
 import {TradeItemService} from './trade-item.service';
@@ -27,9 +27,13 @@ export class TradeItemListComponent implements OnInit {
     this.routerService.navigate([this.routerService.routes.TRADE_ITEM_NEW]);
   }
 
-  private getTradeItems(pageNumber: number) {
+  private getTradeItems(page: Page): void {
+    // return if invalid page number or active page
+    if (page && page.number && page.isActive) {
+      return;
+    }
     this.isLoading = true;
-    let searchResult = this.tradeItemService.search(pageNumber, 10)
+    let searchResult = this.tradeItemService.search(page.number, 10)
       .then(response => {
         this.pagination = response.pagination;
         this.tradeItems = response.data;
@@ -39,7 +43,9 @@ export class TradeItemListComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getTradeItems(1);
+    let page: Page = new Page();
+    page.number = 1;
+    this.getTradeItems(page);
   }
 
   private navigateToTradeItem(tradeItem: TradeItem) {
